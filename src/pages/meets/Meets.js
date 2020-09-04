28b86f6d5774c50f38219fa2387c685f35dc49f8
Andrés García <sneider7896@gmail.com>
2020-09-03 22:37:06 -0500

    Nueva estructura
    Mejora de la estructura de las carpetas para la separación de Componentes, servicios y paginas.

---
+++ src/pages/meets/Meets.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Meets extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/pages/travels/Travels.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Travels extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
