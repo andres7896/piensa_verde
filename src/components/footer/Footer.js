27316a1c2c3d66ea75917f669a706d60fe62ad2f
Andrés García <sneider7896@gmail.com>
2020-09-04 14:50:39 -0500

    Header y Footer terminados
    Se finzalizo el header y el footer, ademas de rediseño de la pagina

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Footer.css'
+import Logo from '../../image/icons/tree.png';
+import Facebook from '../../image/icons/facebook.png';
+import Instagram from '../../image/icons/instagram.png';
+import Twitter from '../../image/icons/twitter.png';
+import Wpp from '../../image/icons/whatsapp.png';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <footer className='footer_container'>
+                <section className='brand'>
+                    <img src={Logo} alt='Logo' />
+                    <div className='copyright'>
+                        <h2><strong> Artemi </strong></h2>
+                        <p><em> Todos los derechos reservados © 2020 </em></p>
+                    </div>
+                </section>
+                <section className='info'>
+                    <div className='members'>
+                        <h3><strong> Integrantes </strong></h3>
+                        <ul>
+                            <li> Carlos Rodríguez </li>
+                            <li> Joham Morales </li>
+                            <li> Andrés García </li>
+                            <li> Miguel Bernal </li>
+                        </ul>
+                    </div>
+                    <div className='social_media'>
+                        <h3> <strong> Social Media </strong></h3>
+                        <img src={Facebook} alt='facebook' />
+                        <img src={Instagram} alt='instagram' />
+                        <img src={Twitter} alt='twitter' />
+                        <img src={Wpp} alt='whatsapp' />
+                    </div>
+                </section>
+            </footer>
         )
     }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,26 +6,28 @@ import { Link } from '@reach/router';
 export default class Header extends Component {
     render() {
         return (
-            <nav>
-                <Link to='/' className='header'>
-                    <img src={ Logo } alt='Logo'/>
-                    <h1> Artemi </h1>
-                </Link>
-                <div className='menu'>
-                    <Link to='/travels' className='btnNav'>
-                        Viajes
+            <div className='header'>
+                <nav>
+                    <Link to='/' className='logo'>
+                        <img src={Logo} alt='Logo' />
+                        <h1> Artemi </h1>
                     </Link>
-                    <Link to='/conferences' className='btnNav'>
-                        Conferencias
+                    <div className='menu'>
+                        <Link to='/travels' className='btnNav'>
+                            Viajes
                     </Link>
-                    <Link to='/contact' className='btnNav'>
-                        Contacto
+                        <Link to='/conferences' className='btnNav'>
+                            Conferencias
                     </Link>
-                    <Link to='/about' className='btnNav'>
-                        Acerca de
+                        <Link to='/contact' className='btnNav'>
+                            Contacto
                     </Link>
-                </div>
-            </nav>
+                        <Link to='/about' className='btnNav'>
+                            Acerca de
+                    </Link>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
