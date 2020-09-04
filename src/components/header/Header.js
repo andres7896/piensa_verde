1cac5f244c113cc17532add851c1c3cb1543bb21
Andrés García <sneider7896@gmail.com>
2020-09-04 02:23:58 -0500

    Navbar finalizado
    Navbar finalizado, pendiente por revisión de estilos

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Header.css';
+import Logo from '../../image/icons/favicon.png';
+import { Link } from '@reach/router';
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <nav>
+                <Link to='/' className='header'>
+                    <img src={ Logo } alt='Logo'/>
+                    <h1> Artemi </h1>
+                </Link>
+                <div className='menu'>
+                    <Link to='/travels' className='btnNav'>
+                        Viajes
+                    </Link>
+                    <Link to='/conferences' className='btnNav'>
+                        Conferencias
+                    </Link>
+                    <Link to='/contact' className='btnNav'>
+                        Contacto
+                    </Link>
+                    <Link to='/about' className='btnNav'>
+                        Acerca de
+                    </Link>
+                </div>
+            </nav>
         )
     }
 }
--- src/services/router.js
+++ src/services/router.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Router } from '@reach/router';
-import LandingPage from '../pages/landing/Landing';
-import ConferencePage from '../pages/meets/Meets';
-import TravelsPage from '../pages/travels/Travels';
+
 import AboutPage from '../pages/about/About';
+import ConferencePage from '../pages/meets/Meets';
 import ContactPage from '../pages/contact/Contact';
-import SuccessPage from '../pages/success/Success';
 import ErrorPage from '../pages/404/Error';
+import LandingPage from '../pages/landing/Landing';
+import SuccessPage from '../pages/success/Success';
+import TravelsPage from '../pages/travels/Travels';
 
 
 const AppRouter = () => (
   <Router>
     <AboutPage path="/about" />
-    <ConferencePage path="conferences"/>
-    <TravelsPage path="travels"/>
+    <ConferencePage path="/conferences" />
     <ContactPage path="/contact" />
-    <SuccessPage path="/success" />
     <ErrorPage path="/404" default />
     <LandingPage path="/" />
+    <SuccessPage path="/success" />
+    <TravelsPage path="/travels" />
   </Router>
 );
 
