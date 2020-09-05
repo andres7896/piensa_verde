38dfb983c0c92c9603eb380aac1547e2c17858b0
Carlos Conde <carlos_iguez27@hotmail.com>
2020-09-04 21:46:32 -0500

    Landing
    Primer landing

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -3,14 +3,83 @@ import './Landing.css'
 
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
-import Cristales from '../../image/places/caño_cristales.jpg';
+
 
 export default class Landing extends Component {
     render() {
         return (
             <div className='container'>
                 <Header />
-                <h1>Hola</h1>
+                <div className="wrapper">
+                    <header className="main-header">
+                        <div className="content">
+                            <h1>A new Generation Smartphone.</h1>
+                            <span>Dual Camera. Clearer Photos</span>
+                            <a href="#" className="start">Get Started</a>
+                        </div>
+                    </header>
+                    <div className="features">
+                        <div className="feature">
+                            <i className="fa fa-bar-chart icon" />
+                            <div className="feature-content">
+                                <h3>Stunning Visual</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <i className="fa fa-code icon" />
+                            <div className="feature-content">
+                                <h3>Clean Code</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <i className="fa fa-gear icon" />
+                            <div className="feature-content">
+                                <h3>More Optimized</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <section id="our-story">
+                        <div className="story-wrapper">
+                            <div className="story-text">
+                                <div>
+                                    <h1>Our Untold Story</h1>
+                                    <span>Improvise</span>
+                                </div>
+                            </div>
+                            <img src="story-bg.jpg" alt="Story" className="story-img" />
+                        </div>
+                        <div className="box">
+                            <h3>From the beginning.</h3>
+                            <br />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque quo molestias vero quas, adipisci consequatur praesentium inventore, laudantium culpa tempore animi, et vitae! Ipsam fugit quo qui tempore a ea.</p>
+                        </div>
+                    </section>
+                    <section className="letter">
+                        <div className="control">
+                            <h3>Subscribe for our newsletter</h3>
+                            <span>Be Updated</span>
+                            <input type="email" placeholder="Your Email" className="input" />
+                            <input type="submit" defaultValue="Subscribe" className="btn" />
+                        </div>
+                    </section>
+                    <section className="contact-wrapper">
+                        <div className="contact">
+                            <i className="fa fa-map-marker" />
+                            <p>Kelley A. Fleming 196 Woodside Circle Mobile, FL 36602</p>
+                        </div>
+                        <div className="contact">
+                            <i className="fa fa-globe" />
+                            <p>something@someone.com</p>
+                        </div>
+                        <div className="contact">
+                            <i className="fa fa-phone" />
+                            <p>0123456789</p>
+                        </div>
+                    </section>
+                </div>
                 <Footer />
             </div>
         )
