1c4b3d96c81738dbd89b8891e11f88c94b7eb248
Andrés García <sneider7896@gmail.com>
2020-09-05 05:08:50 -0500

    Landing finalizado
    Se finalizo el landing y se corrigieron errores de estilos.

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './Landing.css'
+import './Landing.css';
 
-import uno from '../../image/places/1.jpeg'
-import dos from '../../image/places/2.jpg'
-import tres from '../../image/places/3.jpg'
-import cuatro from '../../image/places/4.jpg'
+import Ocean from '../../image/places/ocean.jpeg';
+import Caño from '../../image/places/caño_cristales2.jpg';
+import Laguna from '../../image/places/laguna.jpg';
+import Guatape from '../../image/places/guatape.jpg';
+import Dalai from '../../image/icons/dalai.png'
 
 export default class Landing extends Component {
     render() {
         return (
-            <div className='container'>
+            <div className='container-1'>
                 <div id="slider">
                     <input type="radio" name="slider" id="slide1" defaultChecked />
                     <input type="radio" name="slider" id="slide2" />
@@ -20,22 +21,22 @@ export default class Landing extends Component {
                             <div className="inner">
                                 <div className="slide slide_1">
                                     <div className="slide-content">
-                                        <img src={uno} alt="" />
+                                        <img src={Ocean} alt="Oceano"/>
                                     </div>
                                 </div>
                                 <div className="slide slide_2">
                                     <div className="slide-content">
-                                        <img src={dos} alt="" />
+                                        <img src={Caño} alt="Caño cristales"/>
                                     </div>
                                 </div>
                                 <div className="slide slide_3">
                                     <div className="slide-content">
-                                        <img src={tres} alt="" />
+                                        <img src={Laguna} alt="Laguna"/>
                                     </div>
                                 </div>
                                 <div className="slide slide_4">
                                     <div className="slide-content">
-                                        <img src={cuatro} alt="" />
+                                        <img src={Guatape} alt="Represa de Guatápe"/>
                                     </div>
                                 </div>
                             </div>
@@ -58,31 +59,36 @@ export default class Landing extends Component {
                 <div className="wrapper">
                     <header className="main-header">
                         <div className="content">
-                            <h1>La naturaleza es el sostén de todos los seres y de la vida en el universo</h1>
+                            <h1>"La naturaleza es el sostén de todos los seres y de la vida en el universo"</h1>
                             <span>Dalai Lama</span>
-                            <a href="#" className="start">Get Started</a>
+                            <br></br>
+                            <img src={ Dalai } alt='Dalai Lama' className='dalai_lama'/>
                         </div>
                     </header>
                     <div className="features">
                         <div className="feature">
-                            <i className="fa fa-bar-chart icon" />
                             <div className="feature-content">
-                                <h3>Stunning Visual</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                                <h3>Naturaleza</h3>
+                                <p>"Las enfermedades no nos llegan de la nada. Se desarrollan a partir de pequeños pecados diarios contra la Naturaleza. 
+                                    Cuando se hayan acumulado suficientes pecados, las enfermedades aparecerán de repente."</p>
+                                <br></br>
+                                <span><strong>Hipócrates, Padre de la medicina</strong></span>
                             </div>
                         </div>
                         <div className="feature">
-                            <i className="fa fa-code icon" />
                             <div className="feature-content">
-                                <h3>Clean Code</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                                <h3>Conservación</h3>
+                                <p>"La gente no van a prepociparse por conservación de los animales a menos que piense que los animales merecen la pena."</p>
+                                <br></br>
+                                <span><strong>David attenborough, Naturalista</strong></span>
                             </div>
                         </div>
                         <div className="feature">
-                            <i className="fa fa-gear icon" />
                             <div className="feature-content">
-                                <h3>More Optimized</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam debitis harum eveniet, ea, ipsum necessitatibus?</p>
+                                <h3>Universo</h3>
+                                <p>"El universo no está obligado a estar en perfecta armonía con la ambición humana."</p>
+                                <br></br>
+                                <span><strong>Carl Sagan, Astrónomo</strong></span>
                             </div>
                         </div>
                     </div>
@@ -90,24 +96,16 @@ export default class Landing extends Component {
                         <div className="story-wrapper">
                             <div className="story-text">
                                 <div>
-                                    <h1>Our Untold Story</h1>
-                                    <span>Improvise</span>
+                                    <h1>Artemi - Portal de Piensa Verder</h1>
+                                    <span>Promoviendo el Ecoturismo</span>
                                 </div>
                             </div>
-                            <img src={uno} alt="Story" className="story-img" />
+                            <img src={Ocean} alt="Story" className="story-img" />
                         </div>
                         <div className="box">
-                            <h3>From the beginning.</h3>
+                            <h3>Nuestro Objetivo</h3>
                             <br />
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque quo molestias vero quas, adipisci consequatur praesentium inventore, laudantium culpa tempore animi, et vitae! Ipsam fugit quo qui tempore a ea.</p>
-                        </div>
-                    </section>
-                    <section className="letter">
-                        <div className="control">
-                            <h3>Subscribe for our newsletter</h3>
-                            <span>Be Updated</span>
-                            <input type="email" placeholder="Your Email" className="input" />
-                            <input type="submit" defaultValue="Subscribe" className="btn" />
+                            <p>Promover la conservación del medio ambien en Colombia y el mundo por medio del Ecoturismo.</p>
                         </div>
                     </section>
                 </div>
