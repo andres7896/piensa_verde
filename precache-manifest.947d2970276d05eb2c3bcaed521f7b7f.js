61d735432187fd2391cffa9b72fffde9f9d52844
Andrés García <sneider7896@gmail.com>
2020-09-19 11:18:48 -0500

    Updates

---
+++ precache-manifest.947d2970276d05eb2c3bcaed521f7b7f.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcde59708718f3d516eab88175f21734",
+    "url": "/andres7896.github.io/piensa_verde/index.html"
+  },
+  {
+    "revision": "bd8b1b46cb77802bad8c",
+    "url": "/andres7896.github.io/piensa_verde/static/css/main.5773efa2.chunk.css"
+  },
+  {
+    "revision": "97b13580d5db30e58583",
+    "url": "/andres7896.github.io/piensa_verde/static/js/2.e37f8654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andres7896.github.io/piensa_verde/static/js/2.e37f8654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8b1b46cb77802bad8c",
+    "url": "/andres7896.github.io/piensa_verde/static/js/main.a2f44698.chunk.js"
+  },
+  {
+    "revision": "061b586c67c0bf742b34",
+    "url": "/andres7896.github.io/piensa_verde/static/js/runtime-main.cb9ee01f.js"
+  },
+  {
+    "revision": "5cb19b84716be35f3df7badc1dcde545",
+    "url": "/andres7896.github.io/piensa_verde/static/media/Artemi.5cb19b84.png"
+  },
+  {
+    "revision": "0ea770380185f2292bcf59cb0e5cd277",
+    "url": "/andres7896.github.io/piensa_verde/static/media/alta_guajira.0ea77038.jpg"
+  },
+  {
+    "revision": "1d27880e95b375bb92dc204fb6e86f9e",
+    "url": "/andres7896.github.io/piensa_verde/static/media/amazonas.1d27880e.jpg"
+  },
+  {
+    "revision": "7b74bf44f7fa4eee8e9f66492c18272f",
+    "url": "/andres7896.github.io/piensa_verde/static/media/bosque.7b74bf44.jpg"
+  },
+  {
+    "revision": "c7954c68c6e4c361dd4d8dad36c57996",
+    "url": "/andres7896.github.io/piensa_verde/static/media/caño_cristales.c7954c68.jpg"
+  },
+  {
+    "revision": "9897b6f5fcbb0250e173c42d243023a4",
+    "url": "/andres7896.github.io/piensa_verde/static/media/caño_cristales2.9897b6f5.jpg"
+  },
+  {
+    "revision": "b79313a5b2469fcc77150d69c524bf55",
+    "url": "/andres7896.github.io/piensa_verde/static/media/conservacion.b79313a5.jpg"
+  },
+  {
+    "revision": "52cf217ffe3d2f360a16200a47b79de7",
+    "url": "/andres7896.github.io/piensa_verde/static/media/corota.52cf217f.jpg"
+  },
+  {
+    "revision": "bd012817848c7d33fd89539575c578c8",
+    "url": "/andres7896.github.io/piensa_verde/static/media/dalai.bd012817.png"
+  },
+  {
+    "revision": "efbca274753224755c50fb3e1409e622",
+    "url": "/andres7896.github.io/piensa_verde/static/media/ensenada.efbca274.jpg"
+  },
+  {
+    "revision": "e31a3a136bc497f6b5dc0678c68305f6",
+    "url": "/andres7896.github.io/piensa_verde/static/media/error.e31a3a13.png"
+  },
+  {
+    "revision": "7b4982f279799832dd29410f94f3e9a5",
+    "url": "/andres7896.github.io/piensa_verde/static/media/gorgona.7b4982f2.jpg"
+  },
+  {
+    "revision": "063241b87cb2d38895c29d4796af5ec6",
+    "url": "/andres7896.github.io/piensa_verde/static/media/guatape.063241b8.jpg"
+  },
+  {
+    "revision": "80fe68e1b6602d97ef9c4ffcb1ef984d",
+    "url": "/andres7896.github.io/piensa_verde/static/media/laguna.80fe68e1.jpg"
+  },
+  {
+    "revision": "b225b07233ee2e232c21a516de187973",
+    "url": "/andres7896.github.io/piensa_verde/static/media/message.b225b072.png"
+  },
+  {
+    "revision": "96cc6670cc12f5456962ead0e8aa28f3",
+    "url": "/andres7896.github.io/piensa_verde/static/media/nevado.96cc6670.jpg"
+  },
+  {
+    "revision": "203e1953a3b054b7a8cf73a206331831",
+    "url": "/andres7896.github.io/piensa_verde/static/media/nevados.203e1953.jpg"
+  },
+  {
+    "revision": "db56202d689652df6fd26619e4742a62",
+    "url": "/andres7896.github.io/piensa_verde/static/media/observación.db56202d.jpg"
+  },
+  {
+    "revision": "5b8158cbfe0c620f1b1c0b60ea4b3961",
+    "url": "/andres7896.github.io/piensa_verde/static/media/ocean.5b8158cb.jpeg"
+  },
+  {
+    "revision": "0aff601edda31ecc8864aa55e48a6aa2",
+    "url": "/andres7896.github.io/piensa_verde/static/media/parque_purace.0aff601e.jpg"
+  },
+  {
+    "revision": "4b11f1a950b0b7961b35afcdeb1a93d6",
+    "url": "/andres7896.github.io/piensa_verde/static/media/senderismo.4b11f1a9.jpg"
+  },
+  {
+    "revision": "a2e0a8a2ec63ffcb39f27585fd384f40",
+    "url": "/andres7896.github.io/piensa_verde/static/media/tayrona.a2e0a8a2.jpg"
+  },
+  {
+    "revision": "e2ca1a2444aff434a0c21863502656a2",
+    "url": "/andres7896.github.io/piensa_verde/static/media/think_green.e2ca1a24.png"
+  },
+  {
+    "revision": "35788ee68deaec10d78d91b6bb0f0ff1",
+    "url": "/andres7896.github.io/piensa_verde/static/media/tree.35788ee6.png"
+  },
+  {
+    "revision": "a75ebc5a6da1691b0b51daa50058d3a3",
+    "url": "/andres7896.github.io/piensa_verde/static/media/yoga.a75ebc5a.jpg"
+  }
+]);
